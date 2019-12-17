650e1ee6182acbd7981159afa57857c9365c8263
zusinShinpei <kmh011225@naver.com>
2019-12-17 20:39:41 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "jsxBracketSameLine": false,
+	"jsxSingleQuote": true,
+	"singleQuote": true,
+	"trailingComma": "es5",
+	"useTabs": false,
+	"printWidth": 100,
+	"semi": true
+}
